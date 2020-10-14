81fcec7c7377a2fb9ebeb20aa28c3da905a57c79
techieji <drpradhyum2016@outlook.com>
2020-10-13 21:52:42 -0400

    Added more functions

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
-FROM python:3.8.1-slim
+FROM    python:3.8.1-slim
 WORKDIR /app
-COPY main.py .
-COPY requirements.txt .
-RUN python -m pip install --upgrade pip
-RUN python -m pip install -r requirements.txt
-CMD [ "python", "main.py" ]
+COPY    main.py          .
+COPY    requirements.txt .
+COPY    keys.json        .
+RUN     python -m pip install --upgrade pip
+RUN     python -m pip install -r requirements.txt
+CMD     [ "python", "main.py" ]
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import tweepy
 from textblob import TextBlob
 import json
+from collections import deque
+
+users = deque(maxlen=100)
+rawds = deque(maxlen=100)
+datas = deque()
 
 def setup():
     with open("keys.json") as f:
@@ -9,3 +14,32 @@ def setup():
     auth.set_access_token(stuff["access token"], stuff["access token secret"])
     return tweepy.API(auth)
 
+def rawdata(api: tweepy.API, obj: tweepy.User):
+    return {
+        "user": obj.screen_name,
+        "followers": obj.followers_count,
+        "friends": obj.friends_count,
+        "statuses": obj.statuses_count,
+        # "top10": list(map(lambda x: x.full_text, api.user_timeline(obj, count=10, tweet_mode="extended")))
+        "top10": api.user_timeline(obj)
+    }
+
+def push_user(api: tweepy.API, user: str):
+    obj = api.get_user(user)
+    users.append(obj)
+    rawds.append(rawdata(api, obj))
+
+def expand(api: tweepy.API):
+    user = users.popleft()
+    for follower in api.followers(user):
+        users.append(follower)
+        rawds.append(rawdata(api, follower))
+
+def main():
+    api = setup()
+    push_user(api, "@ezraklein")
+    expand(api)
+    print(list(rawds))
+
+if __name__ == "__main__":
+    main()
